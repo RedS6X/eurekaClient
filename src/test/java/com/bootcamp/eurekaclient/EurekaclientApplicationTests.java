2f8eea2bccd5708793824312409669441f907a74
RedS6X <93229670+RedS6X@users.noreply.github.com>
2022-02-07 16:32:48 +0100

    first commit

---
+++ src/test/java/com/bootcamp/eurekaclient/EurekaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
